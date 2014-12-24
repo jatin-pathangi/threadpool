f13d7e038f7d9d307fcc945eebfb4f982d4eace2
Pathangi Jatinshravan <jatinshravan@gmail.com>
2014-12-23 23:44:38 -0800

    Update threadpool_test.c

--- thread_pool/Test/threadpool_test.c
+++ thread_pool/Test/threadpool_test.c
@@ -17,23 +17,9 @@ void dummy(void *arg)
 
 int main(void)
 {
-  struct mq_attr attr;
-  attr.mq_maxmsg = 10;
-  attr.mq_flags = 0;
-  attr.mq_msgsize = sizeof(struct threadpool_job);
-  attr.mq_curmsgs = 0;
-
   struct threadpool_t *pool;
-  mqd_t m;
   pool = init_threadpool(THREADS);
 
-  mq_unlink(q_name);
- 
-  if((m = mq_open(q_name, O_RDWR | O_CREAT, 0666, &attr)) == -1) {
-    printf("mq_open failed %s\n", strerror(errno));
-    exit(0);
-  }
-  pool->mq = m;
   int i = 0;
   while(i < 15) {
     submit_job(pool, &dummy, NULL);
@@ -41,10 +27,7 @@ int main(void)
   }
   
   threadpool_shutdown(pool);
-  mq_close(m);
-  mq_unlink(q_name);
   
-
   return 0;
 }
 
