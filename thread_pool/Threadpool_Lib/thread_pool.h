e4892586a7f674f8efa19336a9b15634ada1ef33
Pathangi Jatinshravan <jatinshravan@gmail.com>
2014-12-26 14:37:00 -0800

    Update thread_pool.h

--- thread_pool/Threadpool_Lib/thread_pool.h
+++ thread_pool/Threadpool_Lib/thread_pool.h
@@ -1,33 +1,17 @@
-#include <mqueue.h>
-
 #ifndef _THREADPOOL_H
 #define _THREADPOOL_H
 
-#define QUEUE_NAME_LENGTH 10
 
-struct threadpool_t { 
-  pthread_mutex_t lock;
-  pthread_cond_t cond;
-  pthread_t *threads;
-  mqd_t mq;
-  int pending_count;   //Count of the number of pending jobs in the message queue
-  int thread_count;
-  int shutdown;  
-};
+/*Structure to store attributes of the threadpool*/
+struct threadpool_t;
 
-/*Structure for the job given to the threads*/
-struct threadpool_job {     
-  void(*func)(void *);    
-  void *arg;
-};
-
-void get_random_qname(char *qname, int len);
+/*Structure to store the atrributes of a job given to the threadpool*/
+struct threadpool_job;
 
 void *init_threadpool(int num_threads);
 
 int submit_job(void *pool, void(*function)(void *), void *argument);
 
-int free_threadpool(struct threadpool_t *pool);
 
 int threadpool_shutdown(void *pool);
 
