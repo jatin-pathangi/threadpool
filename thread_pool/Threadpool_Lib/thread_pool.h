fc2a476fc4b30082f56ce2d552681f484dfd029b
Pathangi Jatinshravan <jatinshravan@gmail.com>
2014-12-30 18:57:58 -0800

    Update thread_pool.h

--- thread_pool/Threadpool_Lib/thread_pool.h
+++ thread_pool/Threadpool_Lib/thread_pool.h
@@ -1,6 +1,9 @@
 #ifndef _THREADPOOL_H
 #define _THREADPOOL_H
 
+#define IMM_SHUT 1
+#define GRACEFUL_SHUT 2
+#define ABORT_SHUT 3
 
 /*Structure to store attributes of the threadpool*/
 struct threadpool_t;
@@ -13,6 +16,7 @@ void *init_threadpool(int num_threads);
 int submit_job(void *pool, void(*function)(void *), void *argument);
 
 
-int threadpool_shutdown(void *pool);
+int threadpool_shutdown(void *pool, int flag);
 
 #endif
+
